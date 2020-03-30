3ec6a88c247ed2488018791b4ad29781acd59363
Nicola Colic <ncolic@gmail.com>
2020-03-30 22:15:05 +0200

    transorm using oger

---
+++ covid.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import urllib.request
+
+from oger.ctrl.router import Router, PipelineServer
+import glob
+import os
+import json
+
+def get_pmids():
+	url = 'https://www.ncbi.nlm.nih.gov/research/coronavirus-api/export?'
+	urllib.request.urlretrieve(url,'data/pmids.tsv')
+	dataf = pd.read_csv('data/pmids.tsv', sep='\t', comment='#')
+	dataf = dataf['pmid'][~dataf['pmid'].isin(['32150360', '32104909', '32090470'])]
+	dataf.to_csv('data/pmids.txt', sep=' ', index=False, header=False)
+	
+def conll_to_json():
+	pl = PipelineServer(Router())
+	
+	title = ''
+	
+	for f in glob.glob('data/oger/*/*.conll'):
+		pmid = os.path.splitext(os.path.basename(f))[0]
+		if not pmid.startswith('collection'):
+	
+			doc = pl.load_one(f, 'conll')
+			title = doc[0][0].text
+	
+			category = os.path.split(os.path.dirname(f))[-1]
+			directory = os.path.join('data/oger_json/', category)
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+	
+			outfile = os.path.join(directory, pmid + '.json')
+			with open(outfile, 'w', encoding='utf8') as g:
+				pl.write(doc, 'pubanno_json', g)
+			with open(outfile, 'r+', encoding='utf8') as g:
+				bad_json = json.load(g)
+				bad_json['sourcedb'] = 'pubmed'
+				bad_json['sourceid'] = pmid
+	
+				t = bad_json['text']
+				tl = len(title)
+				bad_json['text'] = t[tl:] + ' ' + t[:tl]
+				good_json = bad_json
+				g.seek(0)
+				json.dump(good_json, g)
+
+	
+if __name__== "__main__":
+  get_pmids()
