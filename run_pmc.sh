8680f3e31fbb6c82fb6ce32b05eb51b1e916fcff
Nicola Colic <ncolic@gmail.com>
2020-04-09 12:59:03 +0200

    merged documentation

--- run_pmc.sh
+++ run_pmc.sh
@@ -3,7 +3,7 @@
 home=$(pwd)
 
 echo '0: Creating directories'
-mkdir data data/pmids/ data/oger_pmc/ data/biobert_pmc/ data/harmonised_conll/
+mkdir data data/pmids/ data/oger_pmc/ data/biobert_pmc/ data/harmonised_conll_pmc/
 
 echo '1: Downloading PMIDs'
 python -c 'import covid; covid.pmctsv_to_txt()'
@@ -11,10 +11,10 @@ python -c 'import covid; covid.pmctsv_to_txt()'
 cd home/oger
 for value in CHEBI CL GO_BP GO_CC GO_MF MOP NCBITaxon PR SO UBERON
 do
-echo '2: Running OGER for' $value
-oger run -v -s config/pmc.ini config/$value.ini -o ../data/oger_pmc/$value
-collection=$(ls -t ../data/oger_pmc/$value/*.conll | head -n1)
-cp $collection ../data/oger/$value.conll
+	echo '2: Running OGER for' $value
+	oger run -v -s config/pmc.ini config/$value.ini -o ../data/oger_pmc/$value
+	collection=$(ls -t ../data/oger_pmc/$value/*.conll | head -n1)
+	cp $collection ../data/oger/$value.conll
 done
 
 
@@ -23,7 +23,7 @@ echo '3: Preprocessing for BB'
 python3 biobert_predict.py \
 --do_preprocess=true \
 --input_text=../data/oger_pmc/CL.conll \
---tf_record=../data/biobert_pmc_tokens.tf_record \
+--tf_record=../data/biobert_tokens_pmc.tf_record \
 --vocab_file=common/vocab.txt
 
 declare -A vocabularies=( [CHEBI]=52715 [CL]=52714 [GO_BP]=52715 [GO_CC]=52712 [GO_MF]=52710 [MOP]=52710 [NCBITaxon]=52710 [PR]=52720 [SO]=52714 [UBERON]=52717 )
@@ -39,7 +39,7 @@ mkdir ../data/biobert_pmc/$v-$s
 
 python3 biobert_predict.py \
 	--do_predict=true \
-	--tf_record=../data/biobert_pmc_tokens.tf_record \
+	--tf_record=../data/biobert_tokens_pmc.tf_record \
 	--bert_config_file=common/bert_config.json \
 	--init_checkpoint=models/$v-$s/model.ckpt-${vocabularies[${v}]} \
 	--data_dir=models/$v-$s \
@@ -49,6 +49,19 @@ python3 biobert_predict.py \
 done
 done 
 
+cd home/data/biobert_pmc
+echo '3: Moving BB files'
+for v in CHEBI CL GO_BP GO_CC GO_MF MOP NCBITaxon PR SO UBERON
+do
+for s in ids spans
+do
+echo $v-$s
+mv $v-$s/biobert_tokens_pmc.labels $v-$s.labels
+rm -r $v-$s
+done
+done
+
+
 cd home
 unset vocabularies
 declare -A vocabularies=( [CHEBI]=spans-first [CL]=spans-first [GO_BP]=spans-first [GO_CC]=spans-first [GO_MF]=spans-first [MOP]=spans-first [NCBITaxon]=ids-first [PR]=spans-only [SO]=spans-first [UBERON]=spans-first )
@@ -56,7 +69,8 @@ declare -A vocabularies=( [CHEBI]=spans-first [CL]=spans-first [GO_BP]=spans-fir
 for v in "${!vocabularies[@]}"
 do
 echo '4: Harmonising' $v
-python harmonise.py -t data/harmonised_conll/$v.conll -o data/oger/$v.conll -b data/biobert_tokens/collection.tokens -i data/biobert/$v-ids.labels -s data/biobert/$v-spans.labels -m ${vocabularies[$v]}
+python harmonise.py -t data/harmonised_conll_pmc/$v.conll -o data/oger_pmc/$v.conll -b data/biobert_tokens_pmc.tokens -i data/biobert_pmc/$v-ids.labels -s data/biobert_pmc/$v-spans.labels -m ${vocabularies[$v]}
+done
 
 echo '5: Splitting and .tgz-ing'
 python -c 'import covid; covid.conll_collection_to_jsons()'
